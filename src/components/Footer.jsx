3e5b24accefd9e5c963135156e05cf6fdf84bffd
adarshbhagatjii <bhagatadarsh01@gmail.com>
2025-04-04 16:10:25 +0530

    logo updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full bg-base-300 text-base-content p-4 fixed bottom-0 flex justify-center items-center">
   <aside className="text-center text-sm md:text-base ">
-    <p>Copyright © {new Date().getFullYear()} - All rights reserved by Dev Tinder Industries Ltd</p>
+    <p>Copyright © {new Date().getFullYear()} - All rights reserved by Dev MeetUp Industries Ltd</p>
   </aside>
 </footer>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   const navigate = useNavigate();
 
 
- 
+
   const handelLogout = async () => {
     try {
       await axios.post(BASE_URL + '/logout', {}, {
@@ -21,7 +21,7 @@ const Navbar = () => {
       dispatch(removeUser());
       dispatch(addFeed([]));
       return navigate('/login');
-      
+
     } catch (err) {
       console.log(err);
     }
@@ -31,8 +31,28 @@ const Navbar = () => {
 
   return (
     <div className="navbar bg-gray-300 shadow-lg">
-      <div className="flex-1">
-        <a href='/' className="btn btn-ghost text-xl">Dev Tinder</a>
+      <div className="flex-1 ">
+        <a href='/' className="block max-w-[260px]" ><svg className="w-full h-auto hover:bg-gray-200 rounded-lg" viewBox="0 0 460 120" width="260" height="60" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+          <g transform="translate(20, 30)">
+
+            <circle cx="20" cy="10" r="10" fill="#6B7280" />
+            <path d="M10 30 C10 20, 30 20, 30 30 C30 40, 10 40, 10 30 Z" fill="#6B7280" />
+
+
+            <circle cx="40" cy="10" r="10" fill="#000000" />
+            <path d="M30 30 C30 20, 50 20, 50 30 C50 40, 30 40, 30 30 Z" fill="#000000" />
+
+
+            <path d="M20 30 C22 35, 38 35, 40 30" stroke="#6B7280" stroke-width="3" fill="none" />
+          </g>
+
+
+          <text x="90" y="75" font-family="Segoe UI, sans-serif" font-size="48" fill="#6B7280" font-weight="bold">Dev</text>
+
+
+          <text x="220" y="75" font-family="Segoe UI, sans-serif" font-size="48" fill="#000000" font-weight="bold">MeetUp</text>
+        </svg></a>
       </div>
       {user && (<div className="flex gap-2 items-center">
 
